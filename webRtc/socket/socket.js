7d76a0b479af995822a4260097afa79ddf293f94
Park SeongEun <tjd123123@naver.com>
2021-06-30 02:01:03 +0900

    bbung95
    방 생성/ mongodb 사용 채팅,유저리스트

--- webRtc/socket/socket.js
+++ webRtc/socket/socket.js
@@ -5,8 +5,15 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { v4: uuidV4 } = require("uuid");
 
-////////
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+// for parsing application/json
+app.use(bodyParser.json());
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://localhost:27017/socket", {
   useNewUrlParser: true,
@@ -32,79 +39,63 @@ var list = mongoose.Schema({
   name: "string",
 });
 
-// 7. 정의된 스키마를 객체처럼 사용할 수 있도록 model() 함수로 컴파일
-//var Chat = mongoose.model('Schema', chat);
-
-// 8. Student 객체를 new 로 생성해서 값을 입력
-
-// // 10. Student 레퍼런스 전체 데이터 가져오기
-// Chat.find(function(error, chat){
-//   console.log('--- Read all ---');
-//   if(error){
-//       console.log(error);
-//   }else{
-//       console.log(chat);
-//   }
-// })
-/////////
+var room = mongoose.Schema({
+  roomName: "string",
+});
 
 // 로컬페이지 접속시 기본 출력 페이지
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/socket.html");
+  var Room = mongoose.model("roomlists", room);
+  Room.find(function (err, data) {
+    console.log(data);
+    res.render("list", { data: data });
+  });
 });
 
-// app.get("/room" , (req,res) =>{
-//   res.sendFile(__dirname+"/socket.html");
-// });
+app.post("/create_protocol", (req, res) => {
+  let post = req.body;
+  let Room = mongoose.model("roomlist", room);
+  let roomlist = new Room({ roomName: post.roomName });
+  roomlist.save(function (err, data) {
+    if (err) {
+      console.log(error);
+    } else {
+      console.log("Saved!");
+      res.redirect("/");
+    }
+  });
+});
+
+app.get("/room", (req, res) => {
+  res.render("socket", { roomId: req.query.roomId });
+});
 
 io.on("connection", (user) => {
   // 접속시 io. 커넥션
   console.log("a user connected");
-
-  // room 생성
-  //////////////////////
-  user.on("create", (msg) => {
-    io.emit("create", msg);
-  });
 
   let dis;
   //룸 조인
   user.on("roomjoin", (msg) => {
     dis = msg;
     // room_user list 추가
-    let List = mongoose.model(msg.room + "_user", list);
-    var newList = new List({ name: msg.name });
+    let List = mongoose.model("room_" + msg.room, list);
+    let newList = new List({ name: msg.name });
     newList.save(function (error, data) {
       if (error) {
         console.log(error);
       } else {
         console.log("user 추가");
 
-        loadList(user,msg);
+        loadList(user, msg);
+        loadChat(user, msg);
       }
       console.log(msg.name + "님이 " + msg.room + "에 입장하셨습니다.");
-      io.to(msg.room).emit("roomjoin", msg);
+      //io.to(msg.room).emit("roomjoin", msg);
       user.join(msg.room, () => {});
     });
     ////////////////////////
   });
-
-  // user.on('userlist', (msg) =>{
-  //   user.to(msg.room).emit('userlist', msg);
-  // })
-
-  user.on("roomleave", (msg) => {
-    console.log(msg.name + "님이 " + msg.room + "에서 퇴장하셨습니다.");
-    io.to(msg.room).emit("removelist", msg);
-
-    // mongodb remove
-    removeList(msg);
-    ///////////////
-
-    io.to(msg.room).emit("roomleave", msg);
-    user.leave(msg.room, () => {});
-  });
-  //////////////////////////
 
   // 클라이언트가 보낸 메시지 user.emit
   user.on("chat message", (msg) => {
@@ -119,9 +110,17 @@ io.on("connection", (user) => {
 
   // 접속해제시 socket. 디스커넥션
   user.on("disconnect", () => {
-    removeList(dis);
+    var msg = dis;
+    console.log(msg.name + "님이 " + msg.room + "에서 퇴장하셨습니다.");
+    io.to(msg.room).emit("removelist", msg);
+
+    // mongodb remove
+    removeList(msg);
+    ///////////////
+
+    //io.to(msg.room).emit("roomleave", msg);
+    user.leave(msg.room, () => {});
     console.log("user disconnected");
-    // user.broadcast.emit("chat message", `님이 나가셨습니다.`);
   });
 });
 
@@ -130,8 +129,12 @@ server.listen(3000, () => {
   console.log("listening on * : 3000");
 });
 
+///////////////////////////////////////
+///////////////////////////////////////
+///////////////////////////////////////
+
 function chatList(msg) {
-  var Chat = mongoose.model(msg.room, chat);
+  var Chat = mongoose.model("chat_" + msg.room, chat);
   var newChat = new Chat({ name: msg.name, msg: msg.msg });
   newChat.save(function (error, data) {
     if (error) {
@@ -141,9 +144,20 @@ function chatList(msg) {
     }
   });
 }
-
+function loadChat(user,msg){
+  var Chat = mongoose.model("chat_"+msg.room, chat);
+  Chat.find(function(error,data){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(data);
+      io.to(user.id).emit("loadchat", data);
+      //io.sockets.socket(user.id).emit('loadlist', data);
+    }
+  })
+}
 function loadList(user, msg) {
-  let List = mongoose.model(msg.room + "_user", list);
+  let List = mongoose.model("room_" + msg.room, list);
   List.find(function (error, data) {
     if (error) {
       console.log(error);
@@ -157,13 +171,13 @@ function loadList(user, msg) {
 }
 
 function removeList(msg) {
-  if(msg){
-  let List = mongoose.model(msg.room + "_user", list);
-  List.remove({ name: msg.name }, function (err, output) {
-    console.log("user삭제");
-    if (err) {
-      console.log(error);
-    }
-  });
-}
+  if (msg) {
+    let List = mongoose.model("room_" + msg.room, list);
+    List.remove({ name: msg.name }, function (err, output) {
+      console.log("user삭제");
+      if (err) {
+        console.log(error);
+      }
+    });
+  }
 }
